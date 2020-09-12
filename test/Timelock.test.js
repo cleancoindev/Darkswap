a281a8d3de0d3c4c3e6a91b938a7ec57dafba30e
Ladarken <Sherlockyang22@gmail.com>
2020-09-12 17:19:29 +0800

    Update Timelock.test

--- test/Timelock.test.js
+++ test/Timelock.test.js
@@ -1,7 +1,7 @@
 const { expectRevert, time } = require('@openzeppelin/test-helpers');
 const ethers = require('ethers');
-const SakeToken = artifacts.require('SakeToken');
-const SakeMaster = artifacts.require('SakeMaster');
+const DarkToken = artifacts.require('DarkToken');
+const DarkMaster = artifacts.require('DarkMaster');
 const MockERC20 = artifacts.require('MockERC20');
 const Timelock = artifacts.require('Timelock');
 
@@ -12,23 +12,23 @@ function encodeParameters(types, values) {
 
 contract('Timelock', ([alice, bob, carol, dev, minter]) => {
     beforeEach(async () => {
-        this.sake = await SakeToken.new({ from: alice });
+        this.dark = await DarkToken.new({ from: alice });
         this.timelock = await Timelock.new(bob, '259200', { from: alice });
     });
 
     it('should not allow non-owner to do operation', async () => {
-        await this.sake.transferOwnership(this.timelock.address, { from: alice });
+        await this.dark.transferOwnership(this.timelock.address, { from: alice });
         await expectRevert(
-            this.sake.transferOwnership(carol, { from: alice }),
+            this.dark.transferOwnership(carol, { from: alice }),
             'Ownable: caller is not the owner',
         );
         await expectRevert(
-            this.sake.transferOwnership(carol, { from: bob }),
+            this.dark.transferOwnership(carol, { from: bob }),
             'Ownable: caller is not the owner',
         );
         await expectRevert(
             this.timelock.queueTransaction(
-                this.sake.address, '0', 'transferOwnership(address)',
+                this.dark.address, '0', 'transferOwnership(address)',
                 encodeParameters(['address'], [carol]),
                 (await time.latest()).add(time.duration.days(4)),
                 { from: alice },
@@ -38,55 +38,55 @@ contract('Timelock', ([alice, bob, carol, dev, minter]) => {
     });
 
     it('should do the timelock thing', async () => {
-        await this.sake.transferOwnership(this.timelock.address, { from: alice });
+        await this.dark.transferOwnership(this.timelock.address, { from: alice });
         const eta = (await time.latest()).add(time.duration.days(4));
         await this.timelock.queueTransaction(
-            this.sake.address, '0', 'transferOwnership(address)',
+            this.dark.address, '0', 'transferOwnership(address)',
             encodeParameters(['address'], [carol]), eta, { from: bob },
         );
         await time.increase(time.duration.days(1));
         await expectRevert(
             this.timelock.executeTransaction(
-                this.sake.address, '0', 'transferOwnership(address)',
+                this.dark.address, '0', 'transferOwnership(address)',
                 encodeParameters(['address'], [carol]), eta, { from: bob },
             ),
             "Timelock::executeTransaction: Transaction hasn't surpassed time lock.",
         );
         await time.increase(time.duration.days(4));
         await this.timelock.executeTransaction(
-            this.sake.address, '0', 'transferOwnership(address)',
+            this.dark.address, '0', 'transferOwnership(address)',
             encodeParameters(['address'], [carol]), eta, { from: bob },
         );
-        assert.equal((await this.sake.owner()).valueOf(), carol);
+        assert.equal((await this.dark.owner()).valueOf(), carol);
     });
 
-    it('should also work with SakeMaster', async () => {
+    it('should also work with DarkMaster', async () => {
         this.lp1 = await MockERC20.new('LPToken', 'LP', '10000000000', { from: minter });
         this.lp2 = await MockERC20.new('LPToken', 'LP', '10000000000', { from: minter });
-        this.chef = await SakeMaster.new(this.sake.address, dev, '1000', '0', { from: alice });
-        await this.sake.transferOwnership(this.chef.address, { from: alice });
-        await this.chef.add('100', this.lp1.address, true);
-        await this.chef.transferOwnership(this.timelock.address, { from: alice });
+        this.darklord = await DarkMaster.new(this.dark.address, dev, '1000', '0', { from: alice });
+        await this.dark.transferOwnership(this.darklord.address, { from: alice });
+        await this.darklord.add('100', this.lp1.address, true);
+        await this.darklord.transferOwnership(this.timelock.address, { from: alice });
         const eta = (await time.latest()).add(time.duration.days(4));
         await this.timelock.queueTransaction(
-            this.chef.address, '0', 'set(uint256,uint256,bool)',
+            this.darklord.address, '0', 'set(uint256,uint256,bool)',
             encodeParameters(['uint256', 'uint256', 'bool'], ['0', '200', false]), eta, { from: bob },
         );
         await this.timelock.queueTransaction(
-            this.chef.address, '0', 'add(uint256,address,bool)',
+            this.darklord.address, '0', 'add(uint256,address,bool)',
             encodeParameters(['uint256', 'address', 'bool'], ['100', this.lp2.address, false]), eta, { from: bob },
         );
         await time.increase(time.duration.days(4));
         await this.timelock.executeTransaction(
-            this.chef.address, '0', 'set(uint256,uint256,bool)',
+            this.darklord.address, '0', 'set(uint256,uint256,bool)',
             encodeParameters(['uint256', 'uint256', 'bool'], ['0', '200', false]), eta, { from: bob },
         );
         await this.timelock.executeTransaction(
-            this.chef.address, '0', 'add(uint256,address,bool)',
+            this.darklord.address, '0', 'add(uint256,address,bool)',
             encodeParameters(['uint256', 'address', 'bool'], ['100', this.lp2.address, false]), eta, { from: bob },
         );
-        assert.equal((await this.chef.poolInfo('0')).valueOf().allocPoint, '200');
-        assert.equal((await this.chef.totalAllocPoint()).valueOf(), '300');
-        assert.equal((await this.chef.poolLength()).valueOf(), '2');
+        assert.equal((await this.darklord.poolInfo('0')).valueOf().allocPoint, '200');
+        assert.equal((await this.darklord.totalAllocPoint()).valueOf(), '300');
+        assert.equal((await this.darklord.poolLength()).valueOf(), '2');
     });
 });
