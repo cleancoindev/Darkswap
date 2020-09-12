13e28bd804df2d1bc74de86990111cda44d870ca
Ladarken <Sherlockyang22@gmail.com>
2020-09-12 13:57:02 +0800

    Update Migrator

--- contracts/Migrator.sol
+++ contracts/Migrator.sol
@@ -5,27 +5,27 @@ import "./uniswapv2/interfaces/IUniswapV2Factory.sol";
 
 
 contract Migrator {
-    address public chef;
+    address public darklord;
     address public oldFactory;
     IUniswapV2Factory public factory;
     uint256 public notBeforeBlock;
     uint256 public desiredLiquidity = uint256(-1);
 
     constructor(
-        address _chef,
+        address _darklord,
         address _oldFactory,
         IUniswapV2Factory _factory,
         uint256 _notBeforeBlock
     ) public {
-        require(_chef != address(0) && _oldFactory != address(0) && address(_factory) != address(0), "invalid address");
-        chef = _chef;
+        require(_darklord != address(0) && _oldFactory != address(0) && address(_factory) != address(0), "invalid address");
+        darklord = _darklord;
         oldFactory = _oldFactory;
         factory = _factory;
         notBeforeBlock = _notBeforeBlock;
     }
 
     function migrate(IUniswapV2Pair orig) public returns (IUniswapV2Pair) {
-        require(msg.sender == chef, "not from master chef");
+        require(msg.sender == darklord, "not from the darklord");
         require(block.number >= notBeforeBlock, "too early to migrate");
         require(orig.factory() == oldFactory, "not from old factory");
         address token0 = orig.token0();
