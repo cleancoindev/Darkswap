1c5d35d07938edeb5adaee4ceee51b72b44f05e2
Ladarken <Sherlockyang22@gmail.com>
2020-09-12 15:12:03 +0800

    Update DarkConjuror

--- contracts/DarkConjuror.sol
+++ contracts/DarkConjuror.sol
@@ -7,19 +7,19 @@ import "./uniswapv2/interfaces/IUniswapV2Pair.sol";
 import "./uniswapv2/interfaces/IUniswapV2Factory.sol";
 
 
-contract SakeMaker {
+contract DarkConjuror {
     using SafeMath for uint256;
 
     IUniswapV2Factory public factory;
     address public bar;
-    address public sake;
+    address public dark;
     address public weth;
 
-    constructor(IUniswapV2Factory _factory, address _bar, address _sake, address _weth) public {
+    constructor(IUniswapV2Factory _factory, address _bar, address _dark, address _weth) public {
         require(address(_factory) != address(0) && _bar != address(0) && 
-            _sake != address(0) && _weth != address(0), "invalid address");
+            _dark != address(0) && _weth != address(0), "invalid address");
         factory = _factory;
-        sake = _sake;
+        dark = _dark;
         bar = _bar;
         weth = _weth;
     }
@@ -31,18 +31,18 @@ contract SakeMaker {
         pair.transfer(address(pair), pair.balanceOf(address(this)));
         pair.burn(address(this));
         uint256 wethAmount = _toWETH(token0) + _toWETH(token1);
-        _toSAKE(wethAmount);
+        _toDARK(wethAmount);
     }
 
     function _toWETH(address token) internal returns (uint256) {
-        if (token == sake) {
+        if (token == dark) {
             uint amount = IERC20(token).balanceOf(address(this));
             IERC20(token).transfer(bar, amount);
             return 0;
         }
         if (token == weth) {
             uint amount = IERC20(token).balanceOf(address(this));
-            IERC20(token).transfer(factory.getPair(weth, sake), amount);
+            IERC20(token).transfer(factory.getPair(weth, dark), amount);
             return amount;
         }
         IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(token, weth));
@@ -59,12 +59,12 @@ contract SakeMaker {
         uint amountOut = numerator / denominator;
         (uint amount0Out, uint amount1Out) = token0 == token ? (uint(0), amountOut) : (amountOut, uint(0));
         IERC20(token).transfer(address(pair), amountIn);
-        pair.swap(amount0Out, amount1Out, factory.getPair(weth, sake), new bytes(0));
+        pair.swap(amount0Out, amount1Out, factory.getPair(weth, dark), new bytes(0));
         return amountOut;
     }
 
-    function _toSAKE(uint256 amountIn) internal {
-        IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(weth, sake));
+    function _toDARK(uint256 amountIn) internal {
+        IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(weth, dark));
         (uint reserve0, uint reserve1,) = pair.getReserves();
         address token0 = pair.token0();
         (uint reserveIn, uint reserveOut) = token0 == weth ? (reserve0, reserve1) : (reserve1, reserve0);
