97a4c7610cb970159d9f4539b6bde398ec575fe3
Ladarken <Sherlockyang22@gmail.com>
2020-09-12 17:26:46 +0800

    Update ZGov.test

--- test/ZGovernor.test.js
+++ test/ZGovernor.test.js
@@ -1,7 +1,7 @@
 const { expectRevert, time } = require('@openzeppelin/test-helpers');
 const ethers = require('ethers');
-const SakeToken = artifacts.require('SakeToken');
-const SakeMaster = artifacts.require('SakeMaster');
+const DarkToken = artifacts.require('DarkToken');
+const DarkMaster = artifacts.require('DarkMaster');
 const Timelock = artifacts.require('Timelock');
 const GovernorAlpha = artifacts.require('GovernorAlpha');
 const MockERC20 = artifacts.require('MockERC20');
@@ -13,33 +13,33 @@ function encodeParameters(types, values) {
 
 contract('Governor', ([alice, minter, dev]) => {
     it('should work', async () => {
-        this.sake = await SakeToken.new({ from: alice });
-        await this.sake.delegate(dev, { from: dev });
-        this.chef = await SakeMaster.new(this.sake.address, dev, '100', '0', { from: alice });
-        await this.sake.transferOwnership(this.chef.address, { from: alice });
+        this.dark = await DarkToken.new({ from: alice });
+        await this.dark.delegate(dev, { from: dev });
+        this.darklord = await DarkMaster.new(this.dark.address, dev, '100', '0', { from: alice });
+        await this.dark.transferOwnership(this.darklord.address, { from: alice });
         this.lp = await MockERC20.new('LPToken', 'LP', '10000000000', { from: minter });
         this.lp2 = await MockERC20.new('LPToken2', 'LP2', '10000000000', { from: minter });
-        await this.chef.add('100', this.lp.address, true, { from: alice });
-        await this.lp.approve(this.chef.address, '1000', { from: minter });
-        await this.chef.deposit(0, '100', { from: minter });
-        // Perform another deposit to make sure some SAKEs are minted in that 1 block.
-        await this.chef.deposit(0, '100', { from: minter });
-        assert.equal((await this.sake.totalSupply()).valueOf(), '106');
-        assert.equal((await this.sake.balanceOf(minter)).valueOf(), '100');
-        assert.equal((await this.sake.balanceOf(dev)).valueOf(), '6');
+        await this.darklord.add('100', this.lp.address, true, { from: alice });
+        await this.lp.approve(this.darklord.address, '1000', { from: minter });
+        await this.darklord.deposit(0, '100', { from: minter });
+        // Perform another deposit to make sure some DARKs are minted in that 1 block.
+        await this.darklord.deposit(0, '100', { from: minter });
+        assert.equal((await this.dark.totalSupply()).valueOf(), '100');
+        assert.equal((await this.dark.balanceOf(minter)).valueOf(), '100');
+        assert.equal((await this.dark.balanceOf(dev)).valueOf(), '6');
         // Transfer ownership to timelock contract
         this.timelock = await Timelock.new(alice, time.duration.days(2), { from: alice });
-        this.gov = await GovernorAlpha.new(this.timelock.address, this.sake.address, alice, { from: alice });
+        this.gov = await GovernorAlpha.new(this.timelock.address, this.dark.address, alice, { from: alice });
         await this.timelock.setPendingAdmin(this.gov.address, { from: alice });
         await this.gov.__acceptAdmin({ from: alice });
-        await this.chef.transferOwnership(this.timelock.address, { from: alice });
+        await this.darklord.transferOwnership(this.timelock.address, { from: alice });
         await expectRevert(
-            this.chef.add('100', this.lp2.address, true, { from: alice }),
+            this.darklord.add('100', this.lp2.address, true, { from: alice }),
             'Ownable: caller is not the owner',
         );
         await expectRevert(
             this.gov.propose(
-                [this.chef.address], ['0'], ['add(uint256,address,bool)'],
+                [this.darklord.address], ['0'], ['add(uint256,address,bool)'],
                 [encodeParameters(['uint256', 'address', 'bool'], ['100', this.lp2.address, true])],
                 'Add LP2',
                 { from: alice },
@@ -47,7 +47,7 @@ contract('Governor', ([alice, minter, dev]) => {
             'GovernorAlpha::propose: proposer votes below proposal threshold',
         );
         await this.gov.propose(
-            [this.chef.address], ['0'], ['add(uint256,address,bool)'],
+            [this.darklord.address], ['0'], ['add(uint256,address,bool)'],
             [encodeParameters(['uint256', 'address', 'bool'], ['100', this.lp2.address, true])],
             'Add LP2',
             { from: dev },
@@ -63,6 +63,6 @@ contract('Governor', ([alice, minter, dev]) => {
         await expectRevert(this.gov.execute('1'), "Timelock::executeTransaction: Transaction hasn't surpassed time lock.");
         await time.increase(time.duration.days(3));
         await this.gov.execute('1');
-        assert.equal((await this.chef.poolLength()).valueOf(), '2');
+        assert.equal((await this.darklord.poolLength()).valueOf(), '2');
     });
 });
